0563705e20a9cb5a4b0eed76c7b87e9600910937
mabowen <mabw@bell.ai>
2020-05-29 20:13:59 +0800

    .

---
+++ common-web/src/main/java/com/mbw/commons/web/config/ScheduleConfig.java
@@ -0,0 +1,28 @@
+package com.mbw.commons.web.config;
+
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+/**
+ * 多线程执行定时任务
+ *
+ * @author Mabowen
+ * @date 2020-05-29 09:39
+ */
+@Configuration
+public class ScheduleConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        //设定一个长度10的定时任务线程池
+        ScheduledThreadPoolExecutor poolExecutor = new ScheduledThreadPoolExecutor(10, new BasicThreadFactory.Builder()
+                .namingPattern("sync-schedule-pool-%d")
+                .daemon(true)
+                .build());
+        taskRegistrar.setScheduler(poolExecutor);
+    }
+}
